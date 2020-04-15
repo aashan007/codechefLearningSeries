12150d2abcd3e960182a1dcf2ee89430ce5e723a
I520664 <sarathchandran.s@sap.com>
2020-04-15 20:47:21 +0530

    added test.cpp

---
+++ contest-1/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	while(1){
+	    int t;
+	    cin>>t;
+	    if(t==42){
+	        return 0;
+	    }
+	    cout<<t<<endl;
+	}
+	return 0;
+}
